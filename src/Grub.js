0e1122fbbab06839de4bed9b0e80325b81e0c910
Hack Reactor Students <accounts@hackreactor.com>
2015-02-09 15:29:54 -0800

    Created beehive with pseudoclassical inheritance

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,9 @@
 var Grub = function() {
+  //this = Object.create(Bee.prototype);
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+  //return this;
 };
+Grub.prototype.eat = function(){};
 
